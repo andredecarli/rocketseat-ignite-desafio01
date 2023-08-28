874b104dcb5c1509a4b060163874f555767bc987
André De Carli <a.decarli89@gmail.com>
2023-08-28 14:53:58 -0300

    Add upload from CSV

---
+++ streams/csv-reader.js
@@ -0,0 +1,17 @@
+import { parse } from 'csv-parse';
+import fs from 'node:fs';
+
+(async () => {
+    
+})();
+
+fs.createReadStream("./data.csv")
+        .pipe(parse({delimiter: ',', from_line: 2}))
+        .on("data", function(row) {
+            const task = { title: row[0], description: row[1] };
+            fetch("http://localhost:8080/tasks", {
+                method: 'POST',
+                body: JSON.stringify(task),
+                headers: { 'Content-Type' : 'application/json'},
+            });
+        })
